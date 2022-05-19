6f455a16a686ec0bd81e15f3c6b1d110c926dc62
arifanf <79965177+arifanf@users.noreply.github.com>
2022-05-19 22:04:43 +0700

    Step 2 done

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = (props) => {
+
+  return (
+    <div className="todo-footer">
+        <span className="count-todos">{props.count}</span> { props.count > 1 ? 'items left' : 'item left' }
+        {/* buatlah sebuah ternary operation di mana apabila props count > 1 maka akan menuliskan text "items left" dan jika props count <= 1 maka akan menuliskan "item left" */}
+    </div>
+  )
+}
+
+export default Footer
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import Header from './Header'
 import TodoItem from './TodoItem'
+import Footer from './Footer'
 
 const TodoList = (props) => {
   return (
     <div>
         <div className="todo-list">
-        <Header title={props.title} />
+        <Header title={props.title} addNewItem={props.addNewItem} />
           <ul className="list-group list-group-flush">
           {props.items.map((item, index) => (
             <TodoItem item={item} key={index} />
           ))}
           </ul>
+            <Footer count={props.items.length} />
         </div>
     </div>
   )
