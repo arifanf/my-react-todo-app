466894b54ff7fee374e53b1af946712f7b733b0a
arifanf <79965177+arifanf@users.noreply.github.com>
2022-05-19 22:15:13 +0700

    Step 2 done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import InputBox from './InputBox'
 const Header = (props) => {
   return (
     <>
-      <h1>{props.title}</h1>
+      <h1>{props.title.toUpperCase()}</h1>
       <InputBox addNewItem={props.addNewItem} />
     </>
   )
